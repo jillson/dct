5057587d4aacaa89bfab02899cbaf4c74794de3b
Carleton <carletronicon@gmail.com>
2017-07-31 19:53:33 -0400

    initial models / tests; next up going to add views

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Spot(models.Model):
+    Name = models.CharField(max_length=80,blank=True,null=True)
+    Drop =  models.BooleanField(default=False)
+
+class Visibility(models.Model):
+    Name = models.CharField(max_length=80,blank=True,null=True)
+    Description = models.CharField(max_length=255,blank=True,null=True)
+
+class Row(models.Model):
+    Name = models.CharField(max_length=80,blank=True,null=True)
+    Spots = models.ManyToManyField(Spot)
+    NewRow =  models.BooleanField(default=False)
+    Width = models.IntegerField(default=-1)
+    Visibility = models.ForeignKey(Visibility,null=True)
+    
+class Game(models.Model):
+    Name = models.CharField(max_length=80)
+    Rows = models.ManyToManyField(Row)
+    
+class GameInstance(models.Model):
+    pass
+
+class Card(models.Model):
+    Name = models.CharField(max_length=80)
+    
+class Deck(models.Model):
+    Name = models.CharField(max_length=80)
+    Cards = models.ManyToManyField(Card)
+    
--- cards/tests.py
+++ cards/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+from django.test import TestCase,Client
 
-# Create your tests here.
+from .models import Game, GameInstance, Row, Spot, Card
+
+print ("We read tests.py")
+
+class testGameAPI(TestCase):
+    def test_CreateInvalidGame(self):
+        r = self.client.post("/api/gameInstance/",id=0)
+        self.assertEqual(r.status_code,404)
+    def test_CreateInstance(self):
+        g = Game.objects.create(Name="test")
+        g.save()
+        r = self.client.post("/api/gameInstance/",id=g.id)
+        #self.assertEqual(r.status_code,301)
+        import pdb
+        pdb.set_trace()
+    def test_BadGameDetails(self):
+        r = self.client.get("/api/game/0")
+        self.assertEqual(r.status_code,404)
+    def test_GameInfo(self):
+        g = Game.objects.create(Name="test")
+        g.save()
+        r = self.client.get("/api/game/0")
+        self.assertEqual(r.status_code,200)
+    def test_GameDetails(self):
+        g = Game.objects.create(Name="test")
+        s = Spot.objects.create(Name="a")
+        s2 = Spot.objects.create()
+        s.save()
+        s2.save()
+        aRow = Row.objects.create(Name="aRow",NewRow=True)
+        bRow = Row.objects.create(Name="bRow")
+        aRow.Spots.add(s)
+        bRow.Spots.add(s2)
+        aRow.save()
+        bRow.save()
+        g.Rows.add(aRow)
+        g.Rows.add(bRow)
+        g.save()
+        r = self.client.get("/api/game/"+str(g.id))
+        self.assertContains(r,"test")
+        import pdb
+        pdb.set_trace()
+
+#class testGameInstanceAPI(TestCase):
+#    pass
