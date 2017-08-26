bf17a54a77e7e3f23cea791f4f317069ec6b83a4
Carleton <carletronicon@gmail.com>
2017-08-25 22:25:42 -0400

    update admin to make all our models available (but without any fancification)

--- cards/admin.py
+++ cards/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+admin.site.register(Tag)
+admin.site.register(Card)
+admin.site.register(Deck)
+admin.site.register(Spot)
+admin.site.register(Visibility)
+admin.site.register(Row)
+admin.site.register(Game)
+admin.site.register(GameInstance)
+admin.site.register(Invitation)
