f14578e2dbca095ab7250ae1e625f2bfed5b3a96
Carleton <carletronicon@gmail.com>
2017-08-26 08:35:10 -0400

    update router to support separate chat and update channels (will add game channel next)

--- cards/routing.py
+++ cards/routing.py
@@ -1,6 +1,10 @@
 from channels.routing import route
-from .consumers import ws_message
+from .consumers import ws_connect, ws_message, ws_disconnect, msg_consumer, user_consumer
 
 channel_routing = [
+    route("websocket.connect", ws_connect),
     route("websocket.receive", ws_message),
+    route("websocket.disconnect", ws_disconnect),
+    route("chat-messages", msg_consumer),
+    route("user-messages", user_consumer),
 ]
